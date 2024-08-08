eb725feeb11cbf08e302529a7fbb7a2eeb9c4e2d
madhavivikani <madhavivikani90@gmail.com>
2024-08-08 23:05:12 +0530

    user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+
+  let [get,setget] = useState([]);
+  let [random,setrandom] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://type.fit/api/quotes')
+      .then(function (response) {
+        console.log(response.data);
+        setget(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+      });
+  },[]);
+
+  const btnclick = () =>{
+    const randomIndex = Math.floor(Math.random() * get.length);
+    setrandom(get[randomIndex])
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <p className='title'>Random quote generator</p>
+          <p type='button' className='btnclick' onClick={btnclick}>Generate</p>
+        </div>
+       {
+        random && (
+          <div>
+            <p className='text'>{random.text}</p>
+            <p className='author'>- {random.author}</p>
+          </div>
+        )
+       }
     </div>
   );
 }
